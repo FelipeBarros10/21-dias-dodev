d7aab8738bd7e5247cfdfbbd6283792a80001a40
Felipe Barros <barrosfelipe735@gmail.com>
2024-03-21 01:19:34 -0300

    Dia 9 com exercicios praticos

--- Dia-9/index.js
+++ Dia-9/index.js
@@ -5,7 +5,7 @@ Peso
 Altura
 Profissão
 Informações
-*/
+
 let nome = prompt("Insira seu nome");
 let idade = Number(prompt("Insira sua idade"));
 let peso = Number(prompt("Insira seu peso"));
@@ -27,4 +27,43 @@ if(idade <= 17){
 console.log("Idade em meses: " + idadeEmMeses);
 console.log("Idade em semanas: " + idadeEmSemanas);
 console.log("Idade em dias: " + idadeEmDias);
+*/
 
+// IMC , PESO, ALTURA, NOME, IDADE, Ano atual
+
+/*
+let nome = prompt("Qual seu nome?");
+let idade = Number(prompt("Qual a sua idade?"));
+let anoNascimento = 2024 - idade;
+let peso = prompt("Qual o seu peso?");
+let altura = prompt("Qual a sua altura?");
+let imc  = peso / (altura * altura);
+
+
+if(imc < 18.5){
+    console.log("Seu IMC encontra-se na faixa de magreza");
+}else if(imc < 24,9){
+    console.log("Seu IMC encontra-se na faixa Normal");
+}else if(imc < 30){
+    console.log("Seu IMC encontra-se na faixa Obesidade");
+
+}else{
+    console.log("Seu IMC encontra-se na faixa Obesidade grave")
+}
+
+
+console.log("Seu ano de nascimento é " + anoNascimento );
+*/
+
+let idade = Number(prompt("Qual a sua idade?"))
+let anoAtual = Number(prompt("Em que ano nós estamos?"));
+let anoNascimento = anoAtual - idade;
+let contadorIdadePorAno = 0;
+
+
+for(let contador = anoNascimento; anoNascimento < anoAtual;  anoNascimento++){
+   if(contadorIdadePorAno < idade){
+        console.log(anoNascimento + " - " + contadorIdadePorAno + " anos de idade");
+        contadorIdadePorAno++
+   }
+}
